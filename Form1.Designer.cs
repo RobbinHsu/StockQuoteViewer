a2549fec50755760b45eeee610baa558562d8b7a
RobbinHsu <robbinhsu@gmail.com>
2022-07-21 23:53:44 +0800

    刻UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,58 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.StockName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Amplitude = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stockBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.StockName,
             this.Price,
             this.Amplitude});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView1.Size = new System.Drawing.Size(430, 370);
             this.dataGridView1.TabIndex = 0;
             // 
             // StockName
             // 
+            this.StockName.DataPropertyName = "Name";
             this.StockName.HeaderText = "名稱";
             this.StockName.MinimumWidth = 6;
             this.StockName.Name = "StockName";
+            this.StockName.ReadOnly = true;
             this.StockName.Width = 125;
             // 
             // Price
             // 
+            this.Price.DataPropertyName = "Price";
             this.Price.HeaderText = "成交價";
             this.Price.MinimumWidth = 6;
             this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
             this.Price.Width = 125;
             // 
             // Amplitude
             // 
+            this.Amplitude.DataPropertyName = "Amplitude";
             this.Amplitude.HeaderText = "漲跌";
             this.Amplitude.MinimumWidth = 6;
             this.Amplitude.Name = "Amplitude";
+            this.Amplitude.ReadOnly = true;
             this.Amplitude.Width = 125;
+            // 
+            // stockBindingSource
+            // 
+            this.stockBindingSource.DataSource = typeof(StockQuoteViewer.Stock);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyPress);
+            this.numericUpDown1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.numericUpDown1_MouseDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(463, 441);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -87,8 +134,10 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private BindingSource stockBindingSource;
         private DataGridViewTextBoxColumn StockName;
         private DataGridViewTextBoxColumn Price;
         private DataGridViewTextBoxColumn Amplitude;
+        private NumericUpDown numericUpDown1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,93 @@
+using System.Diagnostics;
+
 namespace StockQuoteViewer
 {
     public partial class Form1 : Form
     {
+        private int _millisecondsDelay;
+        private SynchronizationContext _uiContext;
+        CancellationTokenSource cts = new();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _uiContext = SynchronizationContext.Current;
+
+            BindingStockView();
+            InitialRefreshTime();
+            RefreshView();
+        }
+
+        private void BindingStockView()
+        {
+            var viewSource = new ViewSource();
+            dataGridView1.DataSource = viewSource.Stocks;
+        }
+
+        private void InitialRefreshTime()
+        {
+            _millisecondsDelay = (int)numericUpDown1.Value;
+        }
+
+        private void RefreshView()
+        {
+            Task.Run(async delegate
+            {
+                try
+                {
+                    while (cts.Token.IsCancellationRequested == false)
+                    {
+                        _uiContext.Post(o => { dataGridView1.Refresh(); }, null);
+                        await Task.Delay(_millisecondsDelay, cts.Token);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e);
+                    throw;
+                }
+            }, cts.Token);
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            ReStartView();
+        }
+
+        private void numericUpDown1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReStartView();
+        }
+
+        private void ReStartView()
+        {
+            try
+            {
+                cts.Cancel();
+                cts = new CancellationTokenSource();
+
+                if (numericUpDown1.Value is > 10 and < 300)
+                {
+                    _millisecondsDelay = (int)numericUpDown1.Value;
+                }
+
+                RefreshView();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void numericUpDown1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            ReStartView();
         }
     }
 }
