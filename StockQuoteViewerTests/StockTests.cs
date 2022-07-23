94750e07a75e81b2e2f00255528e25115168fe15
RobbinHsu <robbinhsu@gmail.com>
2022-07-23 08:28:31 +0800

    [Test] 新增測試專案

---
+++ StockQuoteViewerTests/StockTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace StockQuoteViewer.Tests
+{
+    [TestFixture()]
+    public class StockTests
+    {
+        [Test()]
+        public void StockTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
